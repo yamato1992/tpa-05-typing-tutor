36d50fb021ef3bd069720816bbf9ce6cdc89d488
yamato1992 <yamato.mitsui.orleans@gmail.com>
2019-02-01 00:21:35 +0900

    add detect game end and show score feature

--- public/js/components/TypingTutorGame.js
+++ public/js/components/TypingTutorGame.js
@@ -6,6 +6,7 @@ class TypingTutorGame {
     this.isRoundInProgress = false;
     this.currentStrokeCount = -1;
     this.targetText = null;
+    this.correctTypingCount = 0;
   }
 
   init() {
@@ -19,13 +20,38 @@ class TypingTutorGame {
     this.isRoundInProgress = true;
     this.currentStrokeCount = -1;
     this.initTargetText();
+    this.correctTypingCount = 0;
+  }
+
+  endRound() {
+    this.isRoundInProgress = false;
   }
 
   handleKeyStroke(key) {
     if (!this.isRoundInProgress) return;
     this.currentStrokeCount += 1;
     const targetChar = this.targetText[this.currentStrokeCount];
+    this.countCorrectType(key, targetChar);
     this.view.renderKeystroke(key, targetChar);
+    if (this.hasFinishedRound()) {
+      const score = this.calcCorrectTypingRate();
+      this.view.renderTypingScore(score);
+      this.endRound();
+    }
+  }
+
+  countCorrectType(key, targetChar) {
+    if (key === targetChar) {
+      this.correctTypingCount += 1;
+    }
+  }
+
+  hasFinishedRound() {
+    return this.currentStrokeCount === this.targetText.length - 1;
+  }
+
+  calcCorrectTypingRate() {
+    return Math.floor(this.correctTypingCount / this.targetText.length * 100);
   }
 
   initTargetText() {
